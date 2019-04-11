3693876eb98b8c1aa84dec0ae6e695ca8a90344b
hkhv <746392783@qq.com>
2019-04-11 16:17:29 +0800

    fix: update number directive

--- src/app/share/directives/number.ts
+++ src/app/share/directives/number.ts
@@ -1,18 +1,18 @@
 import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: 'input[numbersOnly]'
+    selector: 'input[numbersOnly]'
 })
 export class NumberDirective {
 
-  constructor(private _el: ElementRef) { }
+    constructor(private _el: ElementRef) { }
 
-  @HostListener('input', ['$event']) onInputChange(event) {
-    const initalValue = this._el.nativeElement.value;
-    this._el.nativeElement.value = initalValue.replace(/[^0-9]*/g, '');
-    if ( initalValue !== this._el.nativeElement.value) {
-      event.stopPropagation();
+    @HostListener('input', ['$event']) onInputChange(event) {
+        const initalValue = this._el.nativeElement.value;
+        this._el.nativeElement.value = initalValue.replace(/[^0-9]*/g, '');
+        if (initalValue !== this._el.nativeElement.value) {
+            event.stopPropagation();
+        }
     }
-  }
 
 }
