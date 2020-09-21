ad8a5620401ff32a16d1108240441a694b4ce594
haiwulei <1021391538@qq.com>
2020-09-21 15:09:02 +0800

    fix: pwd limit pattern

--- src/app/popup/_lib/constant.ts
+++ src/app/popup/_lib/constant.ts
@@ -1,6 +1,6 @@
 const nameLimitation = [1, 32];
 const passwordLimitation = [8, 128];
-const pattern = `^[0-9A-z_\\-\\+\\=\\,\\.\\:\\!]{${ passwordLimitation[0] },${ passwordLimitation[1] }}$`;
+const pattern = `^*{${ passwordLimitation[0] },${ passwordLimitation[1] }}$`;
 
 export const WalletInitConstant = {
     nameLimitation,
