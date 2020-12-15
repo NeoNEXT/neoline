7c546d18a02e82fef2cf5338fd63f0bf9756d3d3
haiwulei <1021391538@qq.com>
2020-12-15 15:36:11 +0800

    fix: api data is null

--- src/app/popup/transfer/transfer.service.ts
+++ src/app/popup/transfer/transfer.service.ts
@@ -103,7 +103,7 @@ export class TransferService {
 
     private getBalance(address: string, asset: string): Observable<UTXO[]> {
         return this.http.get(`${this.global.apiDomain}/v1/neo2/address/utxo?address=${address}&asset_id=${asset}`).pipe(map((res) => {
-            return res as UTXO[];
+            return (res || []) as UTXO[];
         }));
     }
 }
