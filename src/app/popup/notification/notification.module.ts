ebefd01db9cb4dc9650521a51b8148d762a09c4e
bao <965001935@qq.com>
2021-04-12 22:30:46 +0800

    feat: neo3 dapi getPublicKey method

--- src/app/popup/notification/notification.module.ts
+++ src/app/popup/notification/notification.module.ts
@@ -13,10 +13,6 @@ import { PopupNoticeAuthComponent } from './authorization/authorization.componen
 import { PopupNoticeInvokeComponent } from './invoke/invoke.component';
 import { PopupNoticeDeployComponent } from './deploy/deploy.component';
 import { PopupNoticeInvokeMultiComponent } from './invoke-multi/invoke-multi.component';
-import { PopupNoticeNeo3TransferComponent } from './neo3Transfer/neo3Transfer.component';
-import { PopupNoticeNeo3InvokeComponent } from './neo3Invoke/neo3Invoke.component';
-import { PopupNoticeNeo3DeployComponent } from './neo3Deploy/neo3Deploy.component';
-import { PopupNoticeNeo3InvokeMultiComponent } from './neo3Invoke-multi/neo3Invoke-multi.component';
 
 @NgModule({
     declarations: [
@@ -28,10 +24,6 @@ import { PopupNoticeNeo3InvokeMultiComponent } from './neo3Invoke-multi/neo3Invo
         PopupNoticeInvokeComponent,
         PopupNoticeInvokeMultiComponent,
         PopupNoticeDeployComponent,
-        PopupNoticeNeo3TransferComponent,
-        PopupNoticeNeo3InvokeComponent,
-        PopupNoticeNeo3DeployComponent,
-        PopupNoticeNeo3InvokeMultiComponent,
     ],
     imports: [
         CommonModule,
