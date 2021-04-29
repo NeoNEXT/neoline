3232b5d85a9baac599c4ae04aa0dc3eb2f443926
bao <965001935@qq.com>
2021-04-29 18:16:10 +0800

    refactor: chain module add ChainId and NetType constant

--- src/app/popup/_lib/chain.ts
+++ src/app/popup/_lib/chain.ts
@@ -5,3 +5,24 @@
  */
 export type ChainType = 'Neo2' | 'Neo3';
 export const ChainTypeGroups = ['Neo2', 'Neo3'];
+
+/**
+ * chainId 1 Neo2 MainNet
+ * chianId 2 Neo2 TestNet
+ * ChainId 3 N3 MainNet
+ * ChainId 4 N3 TestNet
+ *
+ * @export
+ * @enum {number}
+ */
+export enum ChainId {
+    Neo2MainNet = 1,
+    Neo2TestNet = 2,
+    N3MainNet = 3,
+    N3TestNet = 4,
+}
+
+export enum NetType {
+    MianNet = 'MainNet',
+    TestNet = 'TestNet',
+}
