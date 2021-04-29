ed277bde45adb0bca5eb47e6d3d90868e3460e0b
bao <965001935@qq.com>
2021-04-29 19:05:59 +0800

    refactor: configure webpack

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -42,13 +42,40 @@ module.exports = [{
         "neoline": "./extension/neoline/index.ts",
         "dapi": "./extension/dapi/index.ts",
         "common": "./extension/common/index.ts",
-        "data_module": "./extension/common/data_module.ts"
+        "data_module_neo2": "./extension/common/data_module_neo2.ts"
     },
     output: {
         filename: "[name].js",
         path: path.resolve(__dirname, '../dist'),
         libraryTarget: 'window',
         library: 'NEOLine'
+    },
+    devtool: false,
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /.ts$/,
+                use: "ts-loader?configFile=extension/tsconfig.json",
+                exclude: /node_modules/
+            }
+        ]
+    }
+}, {
+    mode: "production",
+    entry: {
+        "neolineN3": "./extension/neoline/neo3.ts",
+        "dapiN3": "./extension/dapi/neo3.ts",
+        "common": "./extension/common/index.ts",
+        "data_module_neo3": "./extension/common/data_module_neo3.ts",
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, '../dist'),
+        libraryTarget: 'window',
+        library: 'NEOLineN3'
     },
     devtool: false,
     resolve: {
