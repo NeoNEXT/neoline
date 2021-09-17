52cca36ac268f1090b99fcc668fb445741f3cd7b
haiwulei <1021391538@qq.com>
2021-09-17 17:29:05 +0800

    fix: update nft request url

--- src/app/core/states/nft.state.ts
+++ src/app/core/states/nft.state.ts
@@ -1,26 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from '../services/http.service';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { GlobalService } from '../services/global.service';
 
 @Injectable()
 export class NftState {
-    nftUrl = 'http://47.110.14.167:8085';
-
-    constructor(private http: HttpService) {}
+    constructor(private http: HttpService, private global: GlobalService) {}
 
     getNfts(address: string): Observable<any> {
-        // address = 'NUN147kJhy5iMmswWdi738PJpNBrgtm4j8';
         return this.http.get(
-            `${this.nftUrl}/v1/neo3/address/nfts?address=${address}`
+            `${this.global.apiDomain}/v1/neo3/address/nfts?address=${address}`
         );
     }
 
     getNftTokens(address: string, contract: string): Observable<any> {
-        // address = 'NUN147kJhy5iMmswWdi738PJpNBrgtm4j8';
-        // contract = '0xb137c83610d3f0331a48d8d6283864120b4f23a1';
         return this.http.get(
-            `${this.nftUrl}/v1/neo3/address/nfttokens?address=${address}&contract=${contract}`
+            `${this.global.apiDomain}/v1/neo3/address/nfttokens?address=${address}&contract=${contract}`
         );
     }
 
@@ -29,10 +24,8 @@ export class NftState {
         contract: string,
         maxId?: number
     ): Observable<any> {
-        // address = 'NUN147kJhy5iMmswWdi738PJpNBrgtm4j8';
-        // contract = '0xb137c83610d3f0331a48d8d6283864120b4f23a1';
         const count = 10;
-        let url = `${this.nftUrl}/v1/neo3/address/nft/transactions?address=${address}&contract=${contract}&count=${count}`;
+        let url = `${this.global.apiDomain}/v1/neo3/address/nft/transactions?address=${address}&contract=${contract}&count=${count}`;
         if (maxId !== -1) {
             url += `&max_id=${maxId}`;
         }
