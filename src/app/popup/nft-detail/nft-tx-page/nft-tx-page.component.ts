05a36937e4cf6e4b634659f2e5f845382e7926e4
haiwulei <1021391538@qq.com>
2022-07-07 17:25:44 +0800

    fix: N3 transactions

--- src/app/popup/nft-detail/nft-tx-page/nft-tx-page.component.ts
+++ src/app/popup/nft-detail/nft-tx-page/nft-tx-page.component.ts
@@ -42,122 +42,92 @@ export class PopupNftTxPageComponent implements OnInit, OnDestroy {
         this.networkId = this.global.n3Network.id;
         this.address = this.neon.address;
         this.txData = [];
-        this.getInTransactions(1);
+        this.getInTransactions();
     }
 
     ngOnDestroy(): void {
         this.txData = [];
     }
 
-    public getInTransactions(page: number) {
-        if (this.currentPage === page) {
-            return;
-        } else {
-            this.currentPage = page;
-        }
+    public getInTransactions() {
         this.loading = true;
         const httpReq1 = this.nftState.getNftTransactions(
             this.neon.address,
             this.nftContract
         );
-        if (page === 1) {
-            this.chrome
-                .getStorage(STORAGE_NAME.transaction)
-                .subscribe((inTxData) => {
-                    if (
-                        inTxData[this.networkId] === undefined ||
-                        inTxData[this.networkId][this.address] === undefined ||
+        this.chrome
+            .getStorage(STORAGE_NAME.transaction)
+            .subscribe((inTxData) => {
+                if (
+                    inTxData[this.networkId] === undefined ||
+                    inTxData[this.networkId][this.address] === undefined ||
+                    inTxData[this.networkId][this.address][this.nftContract] ===
+                        undefined
+                ) {
+                    this.inTransaction = [];
+                } else {
+                    this.inTransaction =
+                        inTxData[this.networkId][this.address][
+                            this.nftContract
+                        ];
+                }
+                const txIdArray = [];
+                this.inTransaction = this.inTransaction.filter(
+                    (item) =>
+                        new Date().getTime() / 1000 - item.block_time <= 7200
+                );
+                this.inTransaction.forEach((item) => {
+                    txIdArray.push(item.txid);
+                });
+                let httpReq2;
+                if (txIdArray.length === 0) {
+                    httpReq2 = new Promise<any>((mResolve) => {
+                        mResolve([]);
+                    });
+                } else {
+                    httpReq2 = this.txState
+                        .getTxsValid(txIdArray, 'Neo3')
+                        .toPromise();
+                }
+                Promise.all([httpReq1, httpReq2]).then((result: any) => {
+                    let txData = result[0] || [];
+                    const txConfirm = result[1] || [];
+                    txConfirm.forEach((item) => {
+                        const tempIndex = this.inTransaction.findIndex(
+                            (e) => e.txid === item
+                        );
+                        if (tempIndex >= 0) {
+                            this.inTransaction.splice(tempIndex, 1);
+                        }
+                    });
+                    if (inTxData[this.networkId] === undefined) {
+                        inTxData[this.networkId] = {};
+                    } else if (
+                        inTxData[this.networkId][this.address] === undefined
+                    ) {
+                        inTxData[this.networkId][this.address] = {};
+                    } else if (
                         inTxData[this.networkId][this.address][
                             this.nftContract
                         ] === undefined
                     ) {
-                        this.inTransaction = [];
+                        inTxData[this.networkId][this.address][
+                            this.nftContract
+                        ] = [];
                     } else {
-                        this.inTransaction =
-                            inTxData[this.networkId][this.address][
-                                this.nftContract
-                            ];
+                        inTxData[this.networkId][this.address][
+                            this.nftContract
+                        ] = this.inTransaction;
                     }
-                    const txIdArray = [];
-                    this.inTransaction = this.inTransaction.filter(
-                        (item) =>
-                            new Date().getTime() / 1000 - item.block_time <=
-                            7200
+                    this.chrome.setStorage(STORAGE_NAME.transaction, inTxData);
+                    this.inTransaction = this.handleLocalTxs(
+                        this.inTransaction
                     );
-                    this.inTransaction.forEach((item) => {
-                        txIdArray.push(item.txid);
-                    });
-                    let httpReq2;
-                    if (txIdArray.length === 0) {
-                        httpReq2 = new Promise<any>((mResolve) => {
-                            mResolve([]);
-                        });
-                    } else {
-                        httpReq2 = this.txState.getTxsValid(txIdArray, 'Neo3').toPromise();
-                    }
-                    Promise.all([httpReq1, httpReq2]).then((result: any) => {
-                        let txData = result[0] || [];
-                        const txConfirm = result[1] || [];
-                        txConfirm.forEach((item) => {
-                            const tempIndex = this.inTransaction.findIndex(
-                                (e) => e.txid === item
-                            );
-                            if (tempIndex >= 0) {
-                                this.inTransaction.splice(tempIndex, 1);
-                            }
-                        });
-                        if (inTxData[this.networkId] === undefined) {
-                            inTxData[this.networkId] = {};
-                        } else if (
-                            inTxData[this.networkId][this.address] === undefined
-                        ) {
-                            inTxData[this.networkId][this.address] = {};
-                        } else if (
-                            inTxData[this.networkId][this.address][
-                                this.nftContract
-                            ] === undefined
-                        ) {
-                            inTxData[this.networkId][this.address][
-                                this.nftContract
-                            ] = [];
-                        } else {
-                            inTxData[this.networkId][this.address][
-                                this.nftContract
-                            ] = this.inTransaction;
-                        }
-                        this.chrome.setStorage(
-                            STORAGE_NAME.transaction,
-                            inTxData
-                        );
-                        this.inTransaction = this.handleLocalTxs(
-                            this.inTransaction
-                        );
-                        if (this.nftContract !== '') {
-                            if (txData.length === 0) {
-                                this.noMoreData = true;
-                            }
-                            txData = this.inTransaction.concat(txData);
-                            this.txData = txData;
-                        } else {
-                            if (txData.length === 0) {
-                                this.noMoreData = true;
-                            }
-                            txData = this.inTransaction.concat(txData);
-                            this.txData = txData;
-                        }
-                        this.loading = false;
-                    });
+                    txData = this.inTransaction.concat(txData);
+                    this.txData = txData;
+                    this.loading = false;
                 });
-        } else {
-            httpReq1.then((res) => {
-                const resultData = res || [];
-                this.txData = this.txData.concat(resultData);
-                if (resultData.length === 0) {
-                    this.noMoreData = true;
-                }
-                this.loading = false;
             });
-        }
     }
 
     handleLocalTxs(txs: any[]): any[] {
