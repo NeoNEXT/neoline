f24f66becbdfae463fa824aff89945ab39cc1e07
haiwulei <1021391538@qq.com>
2022-12-14 18:04:50 +0800

    fix: select dialog style

--- src/app/popup/_dialogs/select/select.dialog.ts
+++ src/app/popup/_dialogs/select/select.dialog.ts
@@ -1,14 +1,11 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-
 import {
   ChromeService,
-  GlobalService,
   AssetState,
   NeonService,
   SettingState,
 } from '@app/core';
-import { Router } from '@angular/router';
 import { ChainType, SelectItem, STORAGE_NAME } from '@popup/_lib';
 
 @Component({
@@ -16,14 +13,9 @@ import { ChainType, SelectItem, STORAGE_NAME } from '@popup/_lib';
   styleUrls: ['select.dialog.scss'],
 })
 export class PopupSelectDialogComponent implements OnInit {
-  public targetOption: string;
-  public rateCurrency: string;
-
   constructor(
     private dialogRef: MatDialogRef<PopupSelectDialogComponent>,
     private chromeSer: ChromeService,
-    private global: GlobalService,
-    private router: Router,
     private assetSer: AssetState,
     private neonService: NeonService,
     private settingState: SettingState,
@@ -35,36 +27,37 @@ export class PopupSelectDialogComponent implements OnInit {
     }
   ) {}
 
-  ngOnInit() {
-    this.rateCurrency = this.assetSer.rateCurrency;
-    this.targetOption = this.data.currentOption;
+  ngOnInit() {}
+
+  getTitle() {
+    switch (this.data.type) {
+      case 'lang':
+        return 'language';
+      case 'currency':
+        return 'currencyUnit';
+      case 'chain':
+        return 'selectAChain';
+    }
   }
 
-  public cancel() {
-    this.dialogRef.close();
-  }
-
-  public select(targetOption: any) {
-    this.targetOption = targetOption;
-    if (this.data.currentOption === targetOption) {
+  select(option: any) {
+    if (this.data.currentOption === option) {
       return;
     }
     switch (this.data.type) {
       case 'lang':
-        this.settingState.changLang(this.targetOption);
-        this.chromeSer.setStorage(STORAGE_NAME.lang, this.targetOption);
-        this.global.snackBarTip('langSetSucc');
+        this.settingState.changLang(option);
+        this.chromeSer.setStorage(STORAGE_NAME.lang, option);
         this.dialogRef.close();
         break;
       case 'currency':
-        this.rateCurrency = this.targetOption;
-        this.assetSer.rateCurrency = this.rateCurrency;
-        this.chromeSer.setStorage(STORAGE_NAME.rateCurrency, this.rateCurrency);
-        this.dialogRef.close(this.targetOption);
+        this.assetSer.rateCurrency = option;
+        this.chromeSer.setStorage(STORAGE_NAME.rateCurrency, option);
+        this.dialogRef.close(option);
         break;
       case 'chain':
-        this.neonService.selectChainType(this.targetOption as ChainType);
-        this.dialogRef.close(this.targetOption);
+        this.neonService.selectChainType(option as ChainType);
+        this.dialogRef.close(option);
     }
   }
 }
