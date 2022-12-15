38bbdc16e8ce3200afbd960811d6f148eab16d07
haiwulei <1021391538@qq.com>
2022-12-15 11:33:55 +0800

    fix: asset-list dialog style

--- src/app/popup/_dialogs/asset/asset.dialog.ts
+++ src/app/popup/_dialogs/asset/asset.dialog.ts
@@ -10,48 +10,38 @@ import { Asset, NftToken } from '@/models/models';
 export class PopupAssetDialogComponent implements OnInit {
   searchValue = '';
   showBalances = [];
-  showNftTokens = [];
   constructor(
     private dialogRef: MatDialogRef<PopupAssetDialogComponent>,
     @Inject(MAT_DIALOG_DATA)
     public data: {
-      balances?: Array<Asset>;
+      balances?: Asset[] | NftToken[];
       selectedId: string;
       isNft?: boolean;
-      nftTokens?: NftToken[];
     }
   ) {}
 
   ngOnInit() {
     this.showBalances = [...(this.data.balances || [])];
-    this.showNftTokens = [...(this.data.nftTokens || [])];
   }
 
-  public select(item) {
-    let index;
-    if (this.data.isNft) {
-      index = this.data.nftTokens.findIndex((m) => m.tokenid === item.tokenid);
-    } else {
-      index = this.data.balances.findIndex((m) => m.asset_id === item.asset_id);
-    }
-    this.dialogRef.close(index);
+  public select(item: Asset | NftToken) {
+    this.dialogRef.close(item);
   }
 
   search() {
     if (!this.searchValue || this.searchValue.trim() === '') {
       this.showBalances = [...(this.data.balances || [])];
-      this.showNftTokens = [...(this.data.nftTokens || [])];
       return;
     }
     const value = this.searchValue.toLowerCase();
     if (this.data.isNft) {
-      this.showNftTokens = this.data.nftTokens.filter(
+      this.showBalances = (this.data.balances as NftToken[]).filter(
         (item) =>
           item.name.toLowerCase().includes(value) ||
           item.tokenid.toLowerCase().includes(value)
       );
     } else {
-      this.showBalances = this.data.balances.filter((item) =>
+      this.showBalances = (this.data.balances as Asset[]).filter((item) =>
         item.symbol.toLowerCase().includes(value)
       );
     }
