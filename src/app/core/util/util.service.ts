fc243f4fadb56b3e90e23278cafb9dcb6c55545f
haiwulei <1021391538@qq.com>
2023-02-15 11:02:42 +0800

    fix: optimize requests

--- src/app/core/util/util.service.ts
+++ src/app/core/util/util.service.ts
@@ -145,7 +145,6 @@ export class UtilServiceState {
     contracts: string[],
     chainType: ChainType
   ): Promise<string[]> {
-    console.log(contracts);
     const rpcUrl =
       chainType === 'Neo2' ? this.n2Network.rpcUrl : this.n3Network.rpcUrl;
     const requestDatas = [];
@@ -167,8 +166,6 @@ export class UtilServiceState {
         requestDatas.push(data);
       }
     });
-    console.log(this.n2AssetSymbol);
-    console.log(symbolsRes);
     if (requestDatas.length === 0) {
       return Promise.resolve(symbolsRes);
     }
@@ -205,13 +202,14 @@ export class UtilServiceState {
   ): Promise<number[]> {
     const rpcUrl =
       chainType === 'Neo2' ? this.n2Network.rpcUrl : this.n3Network.rpcUrl;
-    const requests = [];
-    contracts.forEach((assetId) => {
-      let tempReq;
+    const requestDatas = [];
+    const requestIndexs = [];
+    const decoimalsRes = [];
+    contracts.forEach((assetId, index) => {
       if (chainType === 'Neo2' && this.n2AssetDecimal.has(assetId)) {
-        tempReq = of(this.n2AssetDecimal.get(assetId)).toPromise();
+        decoimalsRes[index] = this.n2AssetDecimal.get(assetId);
       } else if (chainType === 'Neo3' && this.n3AssetDecimal.has(assetId)) {
-        tempReq = of(this.n3AssetDecimal.get(assetId)).toPromise();
+        decoimalsRes[index] = this.n3AssetDecimal.get(assetId);
       } else {
         const data = {
           jsonrpc: '2.0',
@@ -219,44 +217,48 @@ export class UtilServiceState {
           method: 'invokefunction',
           params: [assetId, 'decimals'],
         };
-        tempReq = this.http.rpcPost(rpcUrl, data).toPromise();
+        requestIndexs.push(index);
+        requestDatas.push(data);
       }
-      requests.push(tempReq);
     });
-    return Promise.all([...requests]).then((res) => {
-      const decoimalsRes = [];
-      res.forEach((item, index) => {
-        let decimal: number;
-        if (typeof item === 'number') {
-          decimal = item;
-        }
-        if (item.stack) {
-          decimal = item.stack[0].value;
-          if (item.stack[0].type === 'Integer') {
-            decimal = Number(item.stack[0].value || 0);
+    if (requestDatas.length === 0) {
+      return Promise.resolve(decoimalsRes);
+    }
+    return this.http
+      .rpcPostReturnAllData(rpcUrl, requestDatas)
+      .toPromise()
+      .then((res) => {
+        res.forEach((item, index) => {
+          let decimal = 0;
+          if (item.result.stack) {
+            decimal = item.result.stack[0].value;
+            if (item.result.stack[0].type === 'Integer') {
+              decimal = Number(item.result.stack[0].value || 0);
+            }
+            if (item.result.stack[0].type === 'ByteArray') {
+              const hexstr = u.reverseHex(item.result.stack[0].value);
+              decimal = new BigNumber(hexstr || 0, 16).toNumber();
+            }
           }
-          if (item.stack[0].type === 'ByteArray') {
-            const hexstr = u.reverseHex(item.stack[0].value);
-            decimal = new BigNumber(hexstr || 0, 16).toNumber();
-          }
+          const sourceIndex = requestIndexs[index];
           if (chainType === 'Neo2') {
-            this.n2AssetDecimal.set(contracts[index], decimal);
+            this.n2AssetDecimal.set(contracts[sourceIndex], decimal);
           } else {
-            this.n3AssetDecimal.set(contracts[index], decimal);
+            this.n3AssetDecimal.set(contracts[sourceIndex], decimal);
           }
-        }
-        decoimalsRes.push(decimal);
+          decoimalsRes[sourceIndex] = decimal;
+        });
+        return decoimalsRes;
       });
-      return decoimalsRes;
-    });
   }
 
   getN3NftNames(contracts: string[]): Promise<string[]> {
-    const requests = [];
-    contracts.forEach((assetId) => {
-      let tempReq;
+    const requestDatas = [];
+    const requestIndexs = [];
+    const namesRes = [];
+    contracts.forEach((assetId, index) => {
       if (this.n3AssetName.has(assetId)) {
-        tempReq = of(this.n3AssetName.get(assetId)).toPromise();
+        namesRes[index] = this.n3AssetName.get(assetId);
       } else {
         const data = {
           jsonrpc: '2.0',
@@ -264,25 +266,28 @@ export class UtilServiceState {
           method: 'getcontractstate',
           params: [assetId],
         };
-        tempReq = this.http.rpcPost(this.n3Network.rpcUrl, data).toPromise();
+        requestDatas.push(data);
+        requestIndexs.push(index);
       }
-      requests.push(tempReq);
     });
-    return Promise.all([...requests]).then((res) => {
-      const namesRes = [];
-      res.forEach((item, index) => {
-        let name: string;
-        if (typeof item === 'string') {
-          name = item;
-        }
-        if (item?.manifest?.name) {
-          name = item.manifest.name;
-          this.n3AssetName.set(contracts[index], name);
-        }
-        namesRes.push(name);
+    if (requestDatas.length === 0) {
+      return Promise.resolve(namesRes);
+    }
+    this.http
+      .rpcPostReturnAllData(this.n3Network.rpcUrl, requestDatas)
+      .toPromise()
+      .then((res) => {
+        res.forEach((item, index) => {
+          let name = '';
+          if (item.result?.manifest?.name) {
+            name = item.result.manifest.name;
+          }
+          const sourceIndex = requestIndexs[index];
+          this.n3AssetName.set(contracts[sourceIndex], name);
+          namesRes[sourceIndex] = name;
+        });
+        return namesRes;
       });
-      return namesRes;
-    });
   }
 
   getN3NftProperties(contract: string, tokenids: string[]): Promise<any[]> {
