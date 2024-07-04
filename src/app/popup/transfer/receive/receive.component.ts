8883e73a350133fdcd2405be18c7354a9d024566
haiwulei <1021391538@qq.com>
2024-07-04 15:06:12 +0800

    fix: fix some bugs

--- src/app/popup/transfer/receive/receive.component.ts
+++ src/app/popup/transfer/receive/receive.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '@/app/reduers';
 import { Unsubscribable } from 'rxjs';
-import { ChainType } from '../../_lib';
+import { ChainType, RpcNetwork } from '../../_lib';
 
 declare var QRCode: any;
 
@@ -15,11 +15,23 @@ export class TransferReceiveComponent implements OnDestroy {
   address: string;
   private qrcodeDom;
   chainType: ChainType;
+  network: RpcNetwork;
   constructor(private store: Store<AppState>) {
     const account$ = this.store.select('account');
     this.accountSub = account$.subscribe((state) => {
       this.address = state.currentWallet?.accounts[0]?.address;
       this.chainType = state.currentChainType;
+      switch (this.chainType) {
+        case 'Neo2':
+          this.network = state.n2Networks[state.n2NetworkIndex];
+          break;
+        case 'Neo3':
+          this.network = state.n3Networks[state.n3NetworkIndex];
+          break;
+        case 'NeoX':
+          this.network = state.neoXNetworks[state.neoXNetworkIndex];
+          break;
+      }
       this.initData();
     });
   }
