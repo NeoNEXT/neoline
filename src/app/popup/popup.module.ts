582afacc1172759146dae620e81d55a253c6b4dc
haiwulei <1021391538@qq.com>
2025-02-28 17:46:40 +0800

    fix: claim-gas component

--- src/app/popup/popup.module.ts
+++ src/app/popup/popup.module.ts
@@ -40,6 +40,7 @@ import {
   PopupHomeBackupComponent,
   PopupHomeDappAuthComponent,
   PopupEvmNftsComponent,
+  PopupClaimGasComponent,
 } from './home';
 import { PopupLoginComponent } from './login/login.component';
 import { PopupNewWalletGuideComponent } from './new-wallet-guide/new-wallet-guide.component';
@@ -64,6 +65,7 @@ const POPUP_COMPONENTS = [
   PopupHomeComponent,
   PopupNftsComponent,
   PopupEvmNftsComponent,
+  PopupClaimGasComponent,
   PopupHomeBackupComponent,
   PopupHomeDappAuthComponent,
   PopupNftTokensComponent,
