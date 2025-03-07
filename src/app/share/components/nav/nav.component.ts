bc40c08b97dba3b9d5eb08e599904620c90bd3eb
haiwulei <1021391538@qq.com>
2025-03-07 16:57:14 +0800

    fix: update transfer and bridge style

--- src/app/share/components/nav/nav.component.ts
+++ src/app/share/components/nav/nav.component.ts
@@ -10,13 +10,16 @@ export class NavComponent {
   @Input() title: string;
   @Input() backUrl: string;
   @Input() needTranslate?: boolean = true;
+  @Input() customBack?: boolean = false;
   @Output() backEvent = new EventEmitter();
 
   constructor(private router: Router) {}
 
   back() {
     this.backEvent.emit();
-    if (this.backUrl) {
+    if (this.customBack) {
+      return;
+    } else if (this.backUrl) {
       this.router.navigateByUrl(this.backUrl);
     } else {
       history.go(-1);
