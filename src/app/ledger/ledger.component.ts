5895f34301613a0f13e41b6f60bd648be38c2f16
haiwulei <1021391538@qq.com>
2025-03-12 10:18:20 +0800

    fix: update style

--- src/app/ledger/ledger.component.ts
+++ src/app/ledger/ledger.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ChainType, RpcNetwork } from '../popup/_lib';
 import { Store } from '@ngrx/store';
 import { AppState } from '@/app/reduers';
