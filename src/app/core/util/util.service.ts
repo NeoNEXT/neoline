bcf96c5512cca125cf48038003cc579de1a4287f
haiwulei <1021391538@qq.com>
2025-08-01 15:11:19 +0800

    fix: get nns address

--- src/app/core/util/util.service.ts
+++ src/app/core/util/util.service.ts
@@ -466,22 +466,32 @@ export class UtilServiceState {
       method: 'invokefunction',
       params: [
         NNS_CONTRACT[chainId],
-        'resolve',
+        'ownerOf',
         [
           {
             type: 'String',
             value: domain,
           },
-          {
-            type: 'Integer',
-            value: '16',
-          },
         ],
       ],
     };
     return this.http.rpcPost(this.n3Network.rpcUrl, data).pipe(
-      map((res) => {
-        return this.handleNeo3StackStringValue(res);
+      map((result) => {
+        let address = '';
+        if (result.state === 'HALT' && result.stack?.[0]?.value) {
+          address = result.stack[0].value;
+          if (result.stack[0].type === 'ByteArray') {
+            address = wallet.getAddressFromScriptHash(
+              u.reverseHex(result.stack[0].value)
+            );
+          }
+          if (result.stack[0].type === 'ByteString') {
+            address = wallet.getAddressFromScriptHash(
+              u.reverseHex(base642hex(result.stack[0].value))
+            );
+          }
+        }
+        return address;
       })
     );
   }
